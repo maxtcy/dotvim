0c12408beda7fbae31a94a4c67571ebff52222e1
max_tseng <maxtcy@gmail.com>
2017-08-16 14:30:13 +0800

    env:script adjustment
    1) Decide whether to update ag or not
    2) Reduce stdout log while install tool
    3) ctags, ag, cscope in this script

--- env.sh
+++ env.sh
@@ -4,10 +4,27 @@ echo "-=update repository=-"
 sudo apt-get update &>/dev/null
 echo "-=update Done=-"
 echo "[1] Cscope"
-sudo apt install cscope
+sudo apt install cscope &>/dev/null
 
 echo "[2] ag (somethig like grep, but faster)"
-sudo apt-get install silversearcher-ag
+sudo apt-get install silversearcher-ag &>/dev/null
+
+outstr=$(ag --version)
+# To get ag version
+N=3
+ver=$(echo $outstr | awk -v N=$N '{print $N}')
+#echo $ver
+num=$(echo $ver | awk -F'.' '{print $1}')
+#echo $num
+
+if [ $num > 0 ]
+then
+	echo "New version, No need to update"
+else
+        echo "[******] Need to using wget to install new version"
+        wget http://launchpadlibrarian.net/323511019/silversearcher-ag_2.0.0-1_amd64.deb
+        sudo dpkg -i silversearcher-ag_2.0.0-1_amd64.deb
+fi
 
 echo "[3] ctags"
-sudo apt-get install ctags
+sudo apt-get install ctags &>/dev/null
