d7cea15a03bff62e25336d652d370a5f88788a8f
max_tseng <maxtcy@gmail.com>
2017-08-17 13:43:58 +0800

    env: different steup by different OS

--- env.sh
+++ env.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-
 MY_HOME=$HOME
 ###########################
 echo "[vimrc download]Fetch dotvim.git from github"
 git clone git://github.com/maxtcy/dotvim.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+unamestr="$(uname -s)" #echo $OSTYPE
+case "${unamestr}" in
+	Linux*)
+		ln -s ~/.vim/vimrc ~/.vimrc
+		;;
+	Darwin*)
+		cp ~/.vim/vimrc /usr/share/vim/
+		;;
+	CYGWIN*)
+		;;
+	*)
+		echo "Unknown OS type!! Abort following steps"
+		exit
+		;;
+esac
 ###########################
 sudo add-apt-repository ppa:pgolm/the-silver-searcher
 echo "-=update repository=-"
@@ -33,13 +46,10 @@ sudo apt install cscope &>/dev/null
 echo "[2] ag (somethig like grep, but faster)"
 sudo apt-get install silversearcher-ag &>/dev/null
 
-outstr=$(ag --version)
-# To get ag version
+outstr=$(ag --version) # To get ag version
 N=3
-ver=$(echo $outstr | awk -v N=$N '{print $N}')
-#echo $ver
-num=$(echo $ver | awk -F'.' '{print $1}')
-#echo $num
+ver=$(echo $outstr | awk -v N=$N '{print $N}') #echo $ver
+num=$(echo $ver | awk -F'.' '{print $1}') #echo $num
 
 if [ $num > 0 ]
 then
@@ -49,10 +59,8 @@ else
         wget http://launchpadlibrarian.net/323511019/silversearcher-ag_2.0.0-1_amd64.deb
         sudo dpkg -i silversearcher-ag_2.0.0-1_amd64.deb
 fi
-
 ###########################
 echo "[3] ctags"
 sudo apt-get install ctags &>/dev/null
-
 ###########################
 echo "[End of this Script] DONE!!! "
